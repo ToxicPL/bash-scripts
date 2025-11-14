a610767b61273e715a7f7fce895eaeed3e224d98
ToxicPL <toxicpl00@gmail.com>
2025-11-14 23:34:01 +0100

    Update README.md to include server-monitoring.sh script and enhance description

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ List of all Bash scripts to global usage
 # Script list
 * ping-logger- Script to ping 8.8.8.8 and log if ping failed
 * random-number-generator - Script to generate random numbers and write it to the file
-*auto-update.sh - Auto-update script to pull latest upgrades and log the update time
-
+* server-monitoring.sh - Script to monitor disk CPU and RAM usage and send alert if usage exceeds threshold
 
--- server-monitoring.sh
+++ server-monitoring.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-# Version 1.0 Server monitoring script to check CPU, memory, disk and processes with the highest cpu and memory usage
+# Script to monitor disk CPU and RAM usage and send alert if usage exceeds threshold
+# 14.11.2025 - Drive monitoring added
+
+#List of monitored drives
+PARTITION=("/" "/boot")
+WEBHOOK_URL="https://example.com/webhook" # Replace with your webhook URL
+
+for PART in "${PARTITION[@]}"; do
+    USAGE=$(df -h "$PART" | awk 'NR==2 {gsub("%","",$5); print $5}')
+    if [ $USAGE -gt 60 ] ; then
+        MESSAGE="⚠️Warning: Disk usage on partition $PART is at ${USAGE}%"
+        curl -X POST -H 'Content-type: application/json' --data "{\"content\":\"$MESSAGE\"}" $WEBHOOK_URL
+    fi
+done
